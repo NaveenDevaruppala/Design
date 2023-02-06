b464a495c98315ac52ea7e679373d8a1b0c7cc94
unknown <naveendevaruppala555@gmail.com>
2023-02-06 16:46:53 +0530

    fst commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './Navbar';
+import Nav2 from './Nav2';
+import Nav3 from './Nav3';
+import Nav4 from './Nav4';
+import Completed from './Completed';
+export default function App() {
+  return (
+   <>
+   {/* <Figma/> */}
+   <Navbar/>
+   <Nav2/>
+   <Nav3/>
+   <Nav4/>
+   <Completed/>
+   </>
+         )
+}
